364e8e1452f3af3f81c2e3b74a444d92850d730d
haraphat01 <haraphat@gmail.com>
2023-09-09 19:37:15 +0300

    fixed the error that arise whie building

--- src/app/api/replicateAPI.ts
+++ src/app/api/replicateAPI.ts
@@ -1,15 +1,21 @@
 import Replicate from 'replicate';
 
 // Bind fetch to the window object
-window.fetch = window.fetch.bind(window);
+if (typeof window !== 'undefined') {
+    window.fetch = window.fetch.bind(window);
+  }
 
 const systemPrompt = `"You are a helpful, respectful, and honest academic assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, false information, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."`
 const maxToken = 500
 
 const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN,
-  });
+    auth: process.env.REPLICATE_API_TOKEN || 'default_token',
+});
+
+
+
+
   
 export const runModel = async (prompt: string) => {
   const output = await replicate.run(
